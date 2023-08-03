08b79217b53ee0cfbbaf95ee336350214a5a1970
Sofia Kostina <vip-miss@mail.ru>
2023-08-03 17:44:47 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; // число
+    public int month; // месяц
+    public int year; // год
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String surname; // поле для фамилии
+    public String name; // поле для имени
+    public String patronymic; // поле для отчества
+    public String passport; // паспорт
+    public String phone; // телефон
+    public boolean subscription; // согласие на смс
+    public FormDate birthday;
+
+}
